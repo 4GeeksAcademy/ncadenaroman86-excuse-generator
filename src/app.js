b7777fbe3e3866fa8c1e349d309be77477887b67
Nelson Cadena <32472985+ncadenaroman86@users.noreply.github.com>
2025-06-03 19:54:15 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import "bootstrap";
-import "./style.css";
+function generateExcuse() {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed on', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car', 'my laptop'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during lunch'];
 
+  let excuse =
+    who[Math.floor(Math.random() * who.length)] + ' ' +
+    action[Math.floor(Math.random() * action.length)] + ' ' +
+    what[Math.floor(Math.random() * what.length)] + ' ' +
+    when[Math.floor(Math.random() * when.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  document.getElementById("excuse").innerHTML = excuse;
+}
